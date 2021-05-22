0e9b43622161b0845ad3488442ba9d85e14eec70
KubiiCz <jakub.73@hotmail.com>
2021-05-22 17:53:16 +0200

    ready version

--- src/main.cpp
+++ src/main.cpp
@@ -2,35 +2,37 @@
 
 // připojení potřebných knihoven
 #include <Wire.h>
-#include <AM2320/AM2320.h>
 #include <thingspeak-arduino-master/src/ThingSpeak.h>
 #include <ESP8266WiFi.h>
-#include <DHT.h>
+#include <ESP8266mDNS.h>
 #include <WiFiUdp.h>
+#include <DHT.h>
 #include <NTPClient-master/NTPClient.h>
+
 // inicializace modulu z knihovny
 
 #define DHTPIN 2
-#define typDHT11 DHT11 
-AM2320 senzor;
-DHT mojeDHT(DHTPIN, typDHT11);
+#define DHTType DHT11
+
+DHT mojeDHT(DHTPIN, DHTType);
 
 //----------------  Fill in your credentails   ---------------------
-char ssid[] = "wifiname";             // your network SSID (name) 
-char pass[] = "pass";         // your network password 
-unsigned long myChannelNumber = 0;  // Replace the 0 with your channel number
-const char * myWriteAPIKey = "maykey";    // Paste your ThingSpeak Write API Key between the quotes 
+char ssid[] = "";                   // your network SSID (name)
+char pass[] = "";              // your network password
+unsigned long myChannelNumber = 0;         // Replace the 0 with your channel number
+const char *myWriteAPIKey = ""; // Paste your ThingSpeak Write API Key between the quotes
 //------------------------------------------------------------------
 float teplota;
 float vlhkost;
 const long utcOffsetInSeconds = 3600;
 
-WiFiClient  client;
+WiFiClient client;
 // Define NTP Client to get time
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
 
-void setup() {
+void setup()
+{
   // zahájení komunikace po sériové lince
   // rychlostí 9600 baud
   Serial.begin(9600);
@@ -40,31 +42,34 @@ void setup() {
   timeClient.begin();
 
   // Connect or reconnect to WiFi
-  if(WiFi.status() != WL_CONNECTED){
+  if (WiFi.status() != WL_CONNECTED)
+  {
     Serial.print("Attempting to connect to SSID: ");
     Serial.println(ssid);
-    while(WiFi.status() != WL_CONNECTED){
+    while (WiFi.status() != WL_CONNECTED)
+    {
       WiFi.begin(ssid, pass);
       Serial.print(".");
-      delay(5000);     
-    } 
+      delay(5000);
+    }
     Serial.println("\nConnected.");
   }
 }
 
-void mereni(){
-    float tep = mojeDHT.readTemperature() - 1.5;
+void mereni()
+{
+  float tep = mojeDHT.readTemperature() - 1.5;
   float vlh = mojeDHT.readHumidity();
   ThingSpeak.setField(1, tep);
   ThingSpeak.setField(2, vlh);
   ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
 }
 
-void loop() {
+void loop()
+{
 
   timeClient.update();
   int min = timeClient.getMinutes();
-  Serial.print(min);
 
   switch (min)
   {
@@ -76,16 +81,14 @@ void loop() {
     mereni();
     break;
 
-   case 30:
+  case 30:
     mereni();
     break;
 
   case 45:
     mereni();
-    break; 
+    break;
   }
-  
 
   delay(20000);
 }
-
